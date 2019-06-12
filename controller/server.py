6e134b6e9e020227817d62bf6bc5f3456619945d
正野脩登 <masano@masanoosamutounoMacBook-Pro.local>
2019-06-12 23:35:35 +0900

    made takeoff_land_function and made code brief

--- controller/server.py
+++ controller/server.py
@@ -2,11 +2,25 @@ import socket
 import sys
 import numpy as np
 
-#問題点: go to x y z speedは
 
-#kinectから受け取ったデータ
+#kinectから受け取ったデータを保存
 # [x,y,z,time]
 fr = [0.0,0.0,0.0,0.0] #追加
+
+
+#離着陸を判断するオートマトン
+class takeofflandAutomata():
+    def __init__(self,states,alphabets,transitions,current_state,finalststes):
+        self.states = states
+        self.alphabets = alphabets
+        self.transitions = transitions
+        self.current_state = current_state
+        self.final_states = final_states
+    def run(self,shape):
+        self.current_state = self.transitions[self.current_state][shape]
+    
+
+
 
 def main():
     # 待ち受け用のソケットオブジェクト
@@ -15,9 +29,36 @@ def main():
     host = ''
     port = 9000
     locaddr = (host,port)
-    tello = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   #追加
-    tello_address = ('192.168.10.1', 8889)  #追加
+    tello = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   
+    tello_address = ('192.168.10.1', 8889) 
+    
+    #kinectから受信するデータ
     recieve_message = {"time":-1,"x":0,"y":2,"z":0,"shape":0}
+
+    #離着陸オートマトンの引数
+    states = {"a","b","c","d","e","f","g"}
+    alpabets = {0,1,2}
+    transitions = {
+        a:{0:"b",1:"a",2:"a"},
+        b:{0:"b",1:"c",2:"a"},
+        c:{0:"a",1:"c",2:"d"},
+        d:{0:"e",1:"a",2:"d"},
+        e:{0:"e",1:"f",2:"a"},
+        f:{0:"a",1:"e",2:"g"},
+    }
+    current_state = "a"
+    final_state = "g"
+
+    #takeofflandAutomata classの変数
+    takeoff = takeofflandAutomata(states,alphabets,transitions,current_state,final_state)
+    land = takeofflandAutomata(states,alphabets,transitions,current_state,final_state)
+
+    #空中にいるならsky = 1,地上にいるならsky=0
+    sky = 0
+
+    #ドローンを操作するコマンド
+    move = ['speed', 'cw','ccw','forward 21','up 21','down 21','takeoff','land']
+
     try:
         # 待ち受けポートに割り当て
         tello.bind(locaddr)
@@ -35,12 +76,34 @@ def main():
             recieve_messege = client.recv(4096).decode()
             if recieve_massage['time'] < 0:
                 continue
+            
+            #離陸するならtakeffland = 6, 着陸するならtakeoffland = 7
+            takeoffland = -1
+
+            #地上にいるなら
+            if sky == 0:
+                #手の形を読み取ってオートマトン遷移
+                takeoff.run(recieve_massage['shape'])
+                #離陸
+                if (takeoff.current_state == takeoff_finalstate):
+                    takeoffland = 6
+                    sky = 1
+            #空中にいるなら
+        　　 else:
+                #手の形を読み取ってオートマトン遷移
+                land.run(recieve_massage['shape'])
+                #着陸
+                if (land.current_state == land_finalstate):
+                    takeoffland = 7
+                    sky = 0
+            
             #toは手の動きの相対的な変化を記録する
-            to = []                       #追加↓
+            to = []                       
             to[0] =  recieve_message['x']
             to[1] =  recieve_message['y']
             to[2] =  recieve_message['z']
             to[3] =  recieve_message['time']
+            
             #手がグーなら移動
             if recieve_message['shape']: 
                 #time,x,y,z各々相対的な変化を記録
@@ -52,37 +115,56 @@ def main():
                 forward = int(np.sqrt(x^2+y^2))
                 #z方向に移動する距離
                 updown = abs(z)
-                volb = int((forward+updown)/time)
                 #移動速度
+                volb = int((forward+updown)/time)
                 vol = max(min(speedb,100),10)
-                #移動速度をセット
-                sent = tello.sendto('speed vol'.encode(encoding="utf-8"),tello_addres)
+                
+                #回転角度
+                theta = ''
+                #時計回りなら1,反時計回りなら2
+                rotate = -1
 
-                #xy平面上でドローンの向きを適切に変更
+                #theta,rotateを決定
                 if x >= 0: 
                     theta = int(np.arctan(x/y)*180/np.pi)
-                    sent = tello.sendto('cw theta'.encode(encoding="utf-8"),tello_addres)
+                    rotate = 1
                 else:
                     theta = int(np.arctan(x/y)*180/np.pi)
-                    sent = tello.sendto('ccw theta'.encode(encoding="utf-8"),tello_addres)
+                    rotate = 2
+
                 
                 #最小で20cmまでしか移動できないので、前方向、上下方向の移動する回数を求めている
                 forwardnum = forward/21
                 updownnum = updown/21
 
-                #前方向→上下方向とできるだけ滑らかに移動
+                #上昇するなら4,下降するなら5
+                updown = -1
+
+                #updownを求める
                 if z >= 0:
-                    for i in raneg(0,forwardnum):
-                        sent = tello.sendto('forward 21'.encode(encoding="utf-8"),tello_addres)
-                        if (updown):
-                            sent = tello.sendto('up 21'.encode(encoding="utf-8"),tello_addres)
-                            updown -= 1
+                    updown = 4
                 else:
-                    for i in raneg(0,forwardnum):
-                        sent = ello.sendto('forward 21'.encode(encoding="utf-8"),tello_addres)
-                        if (updown):
-                            sent = tello.sendto('down 21'.encode(encoding="utf-8"),tello_addres)
-                            updown -= 1
+                    updown = 5
+                
+                
+                #離着陸
+                if takeoffland > 0:
+                    sent = tello.sendto('move[takeoffland]'.encode(encoding="utf-8"),tello_addres)
+
+                #向きを変える 
+                sent = tello.sendto('move[0] 100'.encode(encoding="utf-8"),tello_addres)
+                sent = tello.sendto('move[rotate] theta'.encode(encoding="utf-8"),tello_addres)
+
+        　　　　 #スピードを設定
+                sent = tello.sendto('move[0] vol'.encode(encoding="utf-8"),tello_addres)
+
+                #移動
+                for i in raneg(0,forwardnum):
+                    sent = tello.sendto('move[3]'.encode(encoding="utf-8"),tello_addres)
+                    if (updownnum):
+                        sent = tello.sendto('move[updown]'.encode(encoding="utf-8"),tello_addres)
+                        updownnum -= 1
+                
             
             #frの値を更新
             fr[0] = to[0]
