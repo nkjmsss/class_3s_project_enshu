92d3df57258e1bde72aad118d025326b6b9afbc9
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-07-10 14:34:27 +0900

    add: takeoff and land time limit

--- middleware/handler/handler.go
+++ middleware/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -10,6 +11,9 @@ import (
 	"github.com/nkjmsss/class_3s_project_enshu/middleware/models"
 	"github.com/nkjmsss/class_3s_project_enshu/middleware/tcp"
 )
+
+var takeoffTime = time.Date(2001, 5, 20, 23, 59, 59, 0, time.Local)
+var landTime = time.Date(2001, 5, 20, 23, 59, 59, 0, time.Local)
 
 func HandleGet(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World")
@@ -31,14 +35,21 @@ func HandlePost(c echo.Context) error {
 
 	history.Log(r)
 
+	// do nothing after landing for a while
+	if time.Now().After(landTime.Add(5 * time.Second)) {
+		return nil
+	}
+
 	// check if gesture is done
 	if history.DoTakeoff() {
 		d.Command = models.TAKEOFF
-		// fmt.Println("take off!!!")
+		takeoffTime = time.Now()
+		fmt.Println("take off!!!")
 	}
-	if history.DoLand() {
+	if history.DoLand() && time.Now().After(takeoffTime.Add(5*time.Second)) {
 		d.Command = models.LAND
-		// fmt.Println("land!!!")
+		landTime = time.Now()
+		fmt.Println("land!!!")
 	}
 
 	// log.Info("\n" + d.RightHand.String())
